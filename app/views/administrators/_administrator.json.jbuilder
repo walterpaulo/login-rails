0a6d1c18eb53f61278fd1a8d1cb632a70259b14d
walterpaulo <walter0paulo@hotmail.com>
2022-04-25 05:13:01 -0300

    feat: added administrator

---
+++ app/views/administrators/_administrator.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! administrator, :id, :name, :email, :password, :created_at, :updated_at
+json.url administrator_url(administrator, format: :json)
---
+++ db/migrate/20220425081238_create_administrators.rb
@@ -0,0 +1,11 @@
+class CreateAdministrators < ActiveRecord::Migration[7.0]
+  def change
+    create_table :administrators do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
