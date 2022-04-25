a82ba678bdead65ff0f1d2d217ece0caea42e520
walterpaulo <walter0paulo@hotmail.com>
2022-04-25 14:35:58 -0300

    fix: added uuthenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action  :authenticate_user!
+
+  def authenticate_user!
+    return if request.path_parameters[:format] == 'json'
+
+    if cookies[:home_adm].blank?
+      redirect_to "/login"
+    end
+  end
 end
