5270d521767ffe8407df76baf71e26624bf1a939
walterpaulo <walter0paulo@hotmail.com>
2022-04-25 14:38:00 -0300

    feat: methods login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,3 +1,27 @@
 class LoginController < ApplicationController
+    skip_before_action :authenticate_user!
     def index;end
+
+
+  def logar
+     debugger
+      x = ''
+    user = Administrator.where(email: params["login"]["email"], password: params["login"]["password"])
+    if user.present?
+      time = params["login"]["lembrar"] == "1" ? 1.year.from_now : 30.minutes.from_now
+      user = user.first
+      value = {
+        id: user.id,
+        nome: user.name,
+        email: user.email
+      }
+      cookies[:home_adm] = { value: value.to_json, expires: time, httponly: true }
+
+      redirect_to root_path
+    else
+      flash[:erro] = "Email ou senha inválidos"
+      redirect_to login_path
+    end
+  end
+
 end
