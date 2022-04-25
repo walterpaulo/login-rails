abdde18ec78006b9626032faea245b81160c7b15
walterpaulo <walter0paulo@hotmail.com>
2022-04-25 11:54:59 -0300

    fix: new methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :administrators
+  root "administrators#index" 
+
+  get '/login', to: 'login#index'
+  get '/login', to: 'login#logar'
+  get '/logout', to: 'login#logout'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
