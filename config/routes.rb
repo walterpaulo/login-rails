defe4a3e661ecb6463ef2e40f99f2755e4836a4d
walterpaulo <walter0paulo@hotmail.com>
2022-04-25 14:37:10 -0300

    feat: new login route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "administrators#index" 
 
   get '/login', to: 'login#index'
-  get '/login', to: 'login#logar'
+  post '/login', to: 'login#logar'
   get '/logout', to: 'login#logout'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
