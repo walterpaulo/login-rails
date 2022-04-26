7e36578543b3fb4b81be083a5762c7dd6577e56f
walterpaulo <walter0paulo@hotmail.com>
2022-04-25 21:58:25 -0300

    refactor: validates

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,3 +1,8 @@
 class Administrator < ApplicationRecord
-    validates :name, :email, :password, presence: true
+  before_save { self.email = email.downcase }
+  validates :email, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  }
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
 end
